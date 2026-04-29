a6d4bb05ba9dd0f46b1a1408e31f78ad2f4e9daa
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-04-29 16:21:15 +0900

    設定値を共通化 ・プロパティの共通化した項目をコメントアウト ・共通プロパティを読み込み

--- src/main/java/com/example/demo/Sandbox2ndServiceApplication.java
+++ src/main/java/com/example/demo/Sandbox2ndServiceApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@PropertySource("classpath:common.properties")
 @EnableScheduling
 public class Sandbox2ndServiceApplication {
 
